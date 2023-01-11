2c51e2603a60b8023d483bf5df33c907a0253883
Sean Corbett <sean.corbett@umontana.edu>
2023-01-11 23:59:02 +0000

    fixes to scrolling

--- src/components/docs/segments/TerminalSegment.tsx
+++ src/components/docs/segments/TerminalSegment.tsx
@@ -1,7 +1,7 @@
-import { useState, useRef, useLayoutEffect } from 'react';
+
 import Terminal, { ColorMode, TerminalOutput } from 'react-terminal-ui';
-import { useEffect } from 'react';
 import { useInView } from 'react-intersection-observer';
+import { Transition } from "@headlessui/react";
 
 
 const TerminalSegment = ({
@@ -15,19 +15,27 @@ const TerminalSegment = ({
 
   // Terminal has 100% width by default so it should usually be wrapped in a container div
   return (
-    <div className='font-sans text-left text-base flex mt-8'>
-        <div className="h-[100%] w-[100%] flex items-center justify-center" ref={ref}>
-            {
-                inView ?
-                    <Terminal colorMode={ ColorMode.Dark} onInput={null}>
-                        <TerminalOutput>
-                        <div className='terminal-command-segment-wrapper'>
-                            <p className='terminal-segment pl-[2%]'>{command}</p>
-                        </div>
-                    </TerminalOutput>
-                    </Terminal>
-                : <p></p>
-            }
+    <div className='font-sans text-left text-base flex mt-8 h-full' ref={ref}>
+        <div className="h-[100%] w-[100%] flex items-center justify-center" >
+        <Transition
+            as='div'
+            show={inView}
+            className='w-full'
+            enter="transition-opacity duration-[50ms]"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="transition-opacity duration-[50ms]"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+        >
+            <Terminal colorMode={ ColorMode.Dark} onInput={null}>
+                 <TerminalOutput>
+                    <div className='terminal-command-segment-wrapper'>
+                        <p className='terminal-segment pl-[2%]'>{command}</p>
+                    </div>
+                </TerminalOutput>
+            </Terminal>
+        </Transition>
         </div>
     </div>
   )
