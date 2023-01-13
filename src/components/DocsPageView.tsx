5e7ac805e6432909d84dffb89b21c31c44978957
scorbettUM <sean.corbett@umontana.edu>
2023-01-13 09:37:34 -0600

    sc-scroll fix

--- src/components/DocsPageView.tsx
+++ src/components/DocsPageView.tsx
@@ -165,7 +165,6 @@ const DocsPageView = () => {
                         setScrollTimer(null)
                     } 
 
-
                     const scrollY = ref.current?.scrollTop ?? 0;
                     const scrollDistance = Math.abs(scrollY - lastScrollY);
 
