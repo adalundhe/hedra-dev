a86dc7beae3c251ac5d6f9324a6c4f421c09a956
scorbettUM <sean.corbett@umontana.edu>
2023-01-13 01:09:53 -0600

    sc-dev: mobile title card and repo card fixes

--- src/components/banners/AnnouncementBanner.tsx
+++ src/components/banners/AnnouncementBanner.tsx
@@ -5,7 +5,7 @@ const { publicRuntimeConfig } = getConfig()
 
 
 const AnnouncementBanner = () => <Link
-    className='font-creato tracking-wider font-extralight w-screen flex justify-center items-center sticky top-0 bg-[#2e3131] hover:bg-[#2e3131]/80 cursor-pointer text-white font-rany text-md py-4 font-semibold'
+    className='font-creato tracking-wide font-extralight w-screen flex justify-center items-center sticky top-0 bg-[#2e3131] hover:bg-[#2e3131]/80 cursor-pointer text-white font-rany text-md py-4 font-semibold'
     href={`/news/${publicRuntimeConfig.bannerAnnouncementLink}`}
 >
     {publicRuntimeConfig.bannerAnnouncement}
