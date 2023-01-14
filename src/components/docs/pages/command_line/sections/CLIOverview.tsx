27366d0ed6b9d411b068cb35845e36e56b95e56e
Sean Corbett <sean.corbett@umontana.edu>
2023-01-14 20:00:28 +0000

    sc-dev WIP CLI Overview section

--- src/components/docs/pages/command_line/sections/CLIOverview.tsx
+++ src/components/docs/pages/command_line/sections/CLIOverview.tsx
@@ -9,6 +9,14 @@ const CLIOverview = ({
         subSectionName={subSectionName}
         >
         <div>
+            <div>
+                Hedra's command line is the primary way you'll interact with Hedra - from running graphs to communicating 
+                with distributed deployments.
+            </div>
+            <br/>
+            <div>
+                To get started, first 
+            </div>
         </div>
         </Section>
 
